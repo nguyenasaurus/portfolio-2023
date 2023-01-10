89bcb701aae7133b2b10381649f74eaeb70ed62b
nguyenasaurus <justinexnguyen@gmail.com>
2023-01-10 16:17:47 +0000

    feat: add nav styling with dummy text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,12 @@
 
 function Nav() {
   return (
-    <nav className="w-full h-24">
-      
+    <nav className="w-full bg-yellow-400">
+      <ul className="h-24 flex justify-around items-center text-purple-900 font-extrabold">
+        <li className="hover:underline hover:cursor-pointer">Projects</li>
+        <li className="hover:underline hover:cursor-pointer">About</li>
+        <li className="hover:underline hover:cursor-pointer">Get in touch</li>
+      </ul>
     </nav>
   );
 }
